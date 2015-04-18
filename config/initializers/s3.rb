eedc3eaab798c4575a66248263ce8cd2a79224ac
cjshort <callumshorty@hotmail.com>
2015-04-18 20:27:08 +0100

    gem switch

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET'],
-      :endpoint               => "https://s3.amazonaws.com",
-      :region                 => ENV['S3_REGION'] 
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_acl    = :public_read
+  # config.asset_host = 'http://example.com'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
   }
-  config.fog_directory  = ENV['S3_BUCKET']
 end
