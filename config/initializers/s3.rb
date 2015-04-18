07b2d21d6082d05ddeca2fdafc6300f15782e4c6
cjshort <callumshorty@hotmail.com>
2015-04-18 21:10:53 +0100

    region

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET']
+    :aws_region => ENV['REGION']
   }
   config.fog_directory = ENV['S3_BUCKET']
 end
