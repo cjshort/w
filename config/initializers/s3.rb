158f2b555ce9c1003f5731275d855a63c44d6837
cjshort <callumshorty@hotmail.com>
2015-04-19 15:12:22 +0100

    new gem

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'aws',
+    :provider               => 'fog-aws',
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET'],
     :region => ENV['S3_REGION']
