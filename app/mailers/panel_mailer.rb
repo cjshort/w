654501762c48eff2a4f076e70324dbc1a6faa222
cjshort <callumshorty@hotmail.com>
2015-04-20 09:36:32 +0100

    to change

--- app/mailers/panel_mailer.rb
+++ app/mailers/panel_mailer.rb
@@ -7,9 +7,9 @@ class PanelMailer < MandrillMailer::TemplateMailer
     when 1
       to_map = user.humans.map { |human| { email: human.email, name: human.fullname }}
     when 2
-      to_map = user.map { |user| { email: user.email, name: user.firstname+" "+user.lastname }}
+      to_map = { email: user.email, name: user.firstname }
     end
-    
+
     var_humans = user.humans.map { |human| { human.email => {'FULL_NAME' => human.fullname}}}
 
     mandrill_mail template: 'blast-1',
